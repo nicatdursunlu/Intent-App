0e53e6c332e4807fabec1c545c4345a2f103dc84
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-11-28 01:29:00 +0400

    Intent-App

---
+++ app/src/main/java/com/example/intent_app/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.intent_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+    }
+}
